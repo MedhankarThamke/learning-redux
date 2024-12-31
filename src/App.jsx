fa12001222103d2e6ba2561a80261dea53de3d52
MedhankarThamke <tmedhankar@gmail.com>
2024-12-31 14:44:00 +0530

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
-
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>learning redux toolkit</h1>
+      <Todos></Todos>
+      <AddTodo></AddTodo>
     </>
   );
 }
